9cf30b39c77330aa1d19fc68fc5c83a852c4a9e5
Stiven Rodriguez <stiivcode@gmail.com>
2021-03-16 18:34:34 -0500

    update nodejs dsl

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/merq-rodriguez/nodejsapp', 'master') { node ->
+            node / gitConfigName('merq-rodriguez')
+            node / gitConfigEmail('merq.rodriguez@udla.edu.co')
         }
     }
     triggers {
