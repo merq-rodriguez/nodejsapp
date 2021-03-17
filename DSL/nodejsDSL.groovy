67484f3dde4f81e06a911fee083ba0076b3a0545
Stiven Rodriguez <stiivcode@gmail.com>
2021-03-16 19:55:17 -0500

    update in wrappers nodejs name installation for app

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -10,7 +10,7 @@ job('Aplicacion Node.js DSL') {
         scm('H/7 * * * *')
     }
     wrappers {
-        nodejs('nodejs')
+        nodejs('AppNodejs')
     }
     steps {
         shell("npm install")
